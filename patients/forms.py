8b4a1eadb763241ad1281a107c0ae1d2b82c9a2d
Joe McCann <joe.mccann@mailbox.org>
2024-07-27 03:13:40 -0400

    Make DOB a datepicker.

--- patients/forms.py
+++ patients/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Patient
 
 class PatientForm(ModelForm):
   class Meta:
     model = Patient
+    widgets = {
+      'dob': widgets.DateInput(attrs={'type': 'date'})
+    }
     fields = ["first_name", "last_name", "email", "dob", "address", "gender", "primary_practitioner"]
