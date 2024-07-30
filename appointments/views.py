fc498174ed3713d7c10caa9785fb642eb37f2a3d
tola-ogunyale <121975231+tola-ogunyale@users.noreply.github.com>
2024-07-30 15:37:47 -0400

    Add Practitioner Index + Detail view

--- appointments/views.py
+++ appointments/views.py
@@ -1,10 +1,7 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
 from typing import Any
-from django.db.models.query import QuerySet
 from django.views import generic
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from .models import Appointment, TimeTable
+from .models import Appointment, TimeTable, Practitioner
 from appointments.forms import AddAppointmentForm
 
 
@@ -51,3 +48,24 @@ class DeleteAppointmentView(DeleteView):
     template_name = "appointments/modify/delete.html"
     success_url = "/appointments"
     context_object_name = "appointment"
+
+
+#  === practitioner views ==
+class PractitionerIndexView(generic.ListView):
+    template_name = "practitioners/index.html"
+    context_object_name = "practitioners"
+
+    def get_queryset(self):
+        return Practitioner.objects.order_by("full_name")
+
+
+# secondary view on practitioner click:
+class PractitionerDetailView(generic.DetailView):
+    model = Practitioner
+    template_name = "practitioners/detail.html"
+    context_object_name = "practitioner"
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        practitioner = self.get_object()
+        return context
